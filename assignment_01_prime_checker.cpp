d2a7c2ca74cf38bc106cfbbeabc292e5850021e9
Alberta Owusu <aadowusu@st.ug.edu.gh>
2026-08-02 06:46:12 +0000

    Completed assignment one

--- assignment_01_prime_checker.cpp
+++ assignment_01_prime_checker.cpp
@@ -37,3 +37,35 @@
 #include <iostream>
 using namespace std;
 
+// Checks whether a given number is prime
+bool isPrime(int n) {
+    // Numbers less than 2 are NOT prime
+    if (n < 2) {
+        return false;
+    }
+
+    // Check for divisors from 2 up to sqrt(n)
+    for (int i = 2; i * i <= n; i++) {
+        if (n % i == 0) {
+            return false; // found a divisor, not prime
+        }
+    }
+
+    return true; // no divisors found, it's prime
+}
+
+int main() {
+    int number;
+
+    cout << "Enter a number: ";
+    cin >> number;
+
+    if (isPrime(number)) {
+        cout << number << " is a prime number." << endl;
+    } else {
+        cout << number << " is NOT a prime number." << endl;
+    }
+
+    return 0;
+}
+
